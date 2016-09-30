0354b2938fa8e7632ee2f2aa5a159d42525297bd
Mason Salmon <Salmonmasons@aol.com>
2016-09-30 11:48:25 -0400

    Add files via upload

---
+++ Revising the Select Query - 2.sql
@@ -0,0 +1,22 @@
+/* Revising the Select Query - 2
+
+Query the names of all American cities in CITY with populations larger than 120,000. The CountryCode for America is USA.
+
+Input Format
+
+The CITY table is described as follows:
+           CITY              
+*****************************
+*  field     *  Type        *
+*****************************
+*  ID        *  NUMBER      *
+*****************************
+*  NAME      *  VARCHAR2(17)*
+*****************************
+* COUNTRYCODE*  VARCHAR2(17)*
+*****************************
+* DISTRICT   *  VARCHAR2(20)*
+*****************************
+* POPULATION *  NUMBER      *
+******************************/
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = "USA";
